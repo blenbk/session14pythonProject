cc61bc2103d419d52b670a88397fc642a6ccd9dc
blenbk <blenbekuretsion@gmail.com>
2024-03-01 21:55:59 +0100

    Initial commit

---
+++ china.py
@@ -0,0 +1,27 @@
+b = 20
+c = [1,2,3]
+
+prime_numbers = [2, 3, 5, 11, 23, 29]
+non_prime_numbers = [4, 6, 8]
+def is_prime(num):
+    """
+    Checks if a number is prime
+    :param num: the number to check
+    :return: True/False
+    """
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    for i in range(2,num):
+        if num % i == 0:
+            return False
+    return True
+if __name__ == "__main__":
+print("Testing the prime function")
+print("Prime numbers:")
+for i in prime_numbers:
+    print(f"{i} is prime = {is_prime(i)}")
+print("Non prime numbers:")
+for i in non_prime_numbers:
+    print(f"{i} is prime= {is_prime(i)}")
---
+++ main.py
@@ -0,0 +1,22 @@
+# this is the main code
+# Another python file with the name china and write b=20 on the first line
+#import china - simplest way
+from china import is_prime
+from china import is_prime as china_prime
+import china as prc
+from china import *
+from Countries.germany import *
+import Countries.kazakhstan as k
+
+
+a = 10
+is_prime = 7
+#print(china.b)
+#print(china.is_prime(12))
+#print(china.is_prime(2))
+#print(china.is_prime(17))
+print(china_prime(11), is_prime)
+print(prc.is_prime(25))
+print(c)
+print(f"{bmw}, {mercedes}, {audi}")
+print(f"is Borat from Kazakhstan?{k.borat}")
